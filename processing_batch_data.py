f067ffec5631f15d7f73f99b851d33a02b0da380
oli-dev-ai <oopyrchal@pm.me>
2026-06-02 21:08:31 +0200

    converted csv batch to parquet and ipc

--- processing_batch_data.py
+++ processing_batch_data.py
@@ -7,8 +7,4 @@ import sqlite3
 uri = "sqlite://data/database/cardata.db"
 schema_overrides = {"rok_produkcji":pl.String, "data_pierwszej_rej": pl.Date}
 
-df = pl.read_csv(
-    "data/batch/*.csv",
-    schema_overrides=schema_overrides,
-)
-result = df.filter(pl.col("data_pierwszej_rej") > date(2000, 1, 1))
+
